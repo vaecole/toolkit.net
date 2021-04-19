8f58e197268f1b2a5490dfe84b56c9d2c09e857e
Mark - SIAT <ww.wu@siat.ac.cn>
2021-04-19 16:58:36 +0800

    add go init

---
+++ go_demo/main.go
@@ -0,0 +1,12 @@
+// main.go
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, SIATER")
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
