eb71a93f1a2a56b294e38bced6b634deac7c09fd
Mark Wu <markowu@163.com>
2021-05-27 10:28:22 +0800

    submit leetcode664 by python

---
+++ leetcode/src/main/python/main.py
@@ -0,0 +1,13 @@
+import leetcode664
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(leetcode664.Solution().strangePrinter("acbddbcabdaabdbbcdcba")) #"caaabdbbcbccdbcbcdcccabdcdadbccaaaddaaccbadddabca"))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
