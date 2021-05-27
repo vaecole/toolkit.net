154bd8014006995721200520d78dfaa99a3bb23c
Mark Wu <markowu@163.com>
2021-05-27 11:24:20 +0800

    submit leetcode338

--- leetcode/src/main/python/main.py
+++ leetcode/src/main/python/main.py
@@ -1,3 +1,4 @@
+import leetcode338
 import leetcode664
 
 
@@ -8,6 +9,4 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(leetcode664.Solution().strangePrinter("acbddbcabdaabdbbcdcba")) #"caaabdbbcbccdbcbcdcccabdcdadbccaaaddaaccbadddabca"))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(leetcode338.Solution().countBits(16))
