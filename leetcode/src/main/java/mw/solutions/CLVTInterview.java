3060c447b3b1b1d752be6a8748b92eb01751ec29
Mark Wu <markowu@163.com>
2021-06-12 11:48:42 +0800

    add CLVTInterview

---
+++ leetcode/src/main/java/mw/solutions/CLVTInterview.java
@@ -0,0 +1,41 @@
+package mw.solutions;
+
+import java.util.List;
+/*
+ *
+ */
+
+public class CLVTInterview extends SolutionBase {
+
+    /*
+     * Complete the 'getMaxColors' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY prices
+     *  2. INTEGER money
+     */
+
+    public int getMaxColors(List<Integer> prices, int money) {
+        // Write your code here
+        int max = 0;
+        int sum = 0;
+        for (int i = 0, j = 0; i <= j && j < prices.size(); ) {
+            while (sum < money && j < prices.size()) {
+                sum += prices.get(j);
+                j++;
+            }
+
+            int cmax = (j - i - 1) + (sum <= money ? 1 : 0);
+            if (cmax > max) {
+                max = cmax;
+            }
+            while (sum >= money) {
+                sum -= prices.get(i);
+                i++;
+            }
+        }
+        return max;
+    }
+
+}
